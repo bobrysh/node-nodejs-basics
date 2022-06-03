0cb52750eec646e87e05ab3993e6735a5bee197e
Pavel_Bobryshev <Pavel_Bobryshev@epam.com>
2022-06-03 13:53:56 +0300

    args done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
 export const parseArgs = () => {
-    // Write your code here 
+    let string = [];
+    const inputArgs = process.argv.slice(2, process.argv.length);
+    inputArgs.forEach((arg, index) => {
+      if (arg.slice(0, 2).includes('--')) {
+        const longArgKey = arg.slice(2, arg.length);
+        const longArgValue = inputArgs[index + 1]; 
+        string[index] = `${longArgKey} is ${longArgValue}`
+      }
+    });
+    console.log(string.join('\n'));
 };
+
+parseArgs()
