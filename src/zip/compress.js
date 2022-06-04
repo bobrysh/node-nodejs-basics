9116bb83f6bd8c0223502eef2faaa58d91b1d680
Pavel_Bobryshev <Pavel_Bobryshev@epam.com>
2022-06-04 21:09:20 +0300

    compress / decompress added

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,13 @@
-export const compress = async () => {
-    // Write your code here 
+import * as fs from 'fs';
+import * as zlib from 'zlib';
+
+
+export const compress = async (err) => {
+    if (err) console.log("FS operation failed",err)
+    const gzip = zlib.createGzip();   
+    const inp = fs.createReadStream('src/zip/files/fileToCompress.txt');   
+    const out = fs.createWriteStream('src/zip/archive.gz');   
+    inp.pipe(gzip).pipe(out);  
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,13 @@
-export const decompress = async () => {
-    // Write your code here 
+import * as fs from 'fs';
+import * as zlib from 'zlib';
+
+
+export const decompress = async (err) => {
+    if (err) console.log("FS operation failed",err)
+    const unzip = zlib.createUnzip();     
+    const inp = fs.createReadStream('src/zip/archive.gz');   
+    const out = fs.createWriteStream('src/zip/files/fileToCompress.txt');   
+    inp.pipe(unzip).pipe(out);   
 };
+
+decompress();
